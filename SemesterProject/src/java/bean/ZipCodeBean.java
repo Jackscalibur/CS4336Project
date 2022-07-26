ab6b87cd597bea69a5c09a88be7dfaabc5259477
Jackson Cowdrey <jackson.taylor.cowdrey@gmail.com>
2022-07-25 19:47:34 -0500

    update SemesterProject

--- SemesterProject/src/java/bean/ZipCodeBean.java
+++ SemesterProject/src/java/bean/ZipCodeBean.java
@@ -5,10 +5,39 @@
  */
 package bean;
 
+import ejb.MovieAndTheaterEJB;
+import javax.inject.Named;
+import javax.enterprise.context.SessionScoped;
+import java.io.Serializable;
+import javax.ejb.EJB;
+
 /**
  *
  * @author jacks
  */
-public class ZipCodeBean {
+@Named(value = "zipCodeBean")
+@SessionScoped
+public class ZipCodeBean implements Serializable {
     
+    @EJB
+    private MovieAndTheaterEJB movieAndTheaterEJB;
+    private String zip;
+    
+    public ZipCodeBean() {}
+
+    public MovieAndTheaterEJB getMovieAndTheaterEJB() {
+        return movieAndTheaterEJB;
+    }
+
+    public void setMovieAndTheaterEJB(MovieAndTheaterEJB movieAndTheaterEJB) {
+        this.movieAndTheaterEJB = movieAndTheaterEJB;
+    }
+
+    public String getZip() {
+        return zip;
+    }
+
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
 }
